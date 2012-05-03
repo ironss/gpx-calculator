5603b7c256c72abfbba14d32cc262ebb81b22100
Stephen Irons <stephen.irons@clear.net.nz>
2012-05-03 21:42:36 +1200

    Calculate where a rounded point has to go, and how far along the relevant segment.

--- gpx.lua
+++ gpx.lua
@@ -58,9 +58,15 @@ function time_from_iso8601_str(str)
 end
 
 
-function calculate_points(trkseg)
+function calculate_points(trkseg, t_interval, d_interval)
    local tp1 = {}
    local i = 1
+   local total_d = 0
+   local partial_d = 0
+   local rounded_d = d_interval
+   local total_t = 0
+   local partial_t = 0
+   
    for trkpt in trkseg:nodes("trkpt") do
       if i == 1 then
          tp1.lat = rad_from_deg(trkpt.lat)
@@ -73,8 +79,20 @@ function calculate_points(trkseg)
          tp2.time = time_from_iso8601_str(trkpt:find("time")[1])
       
          if tp2.time ~= tp1.time then
-            local distance = f(tp2.lat, tp2.lon, tp1.lat, tp1.lon, spheroid)
-            print(tp2.time, tp2.time - tp1.time, distance)
+            local delta_t = tp2.time - tp1.time
+-- TODO: Calculate bearing            local bearing = 
+            local delta_d = f(tp2.lat, tp2.lon, tp1.lat, tp1.lon, spheroid)
+            
+            if rounded_d >= total_d and rounded_d < total_d + delta_d then
+               -- Rounded point is this far along this segment
+               partial_d = rounded_d - total_d
+               print(tp2.time, "*", "*", partial_d, rounded_d)
+               rounded_d = rounded_d + d_interval
+            end
+            print(tp2.time, total_t, delta_t, total_t + delta_t, total_d, delta_d, total_d+delta_d)
+
+            total_d = total_d + delta_d
+            total_t = total_t + delta_t
          end
          
          tp1 = tp2
@@ -87,7 +105,7 @@ end
 for trk in gpx:nodes("trk") do
    trk.name = trk:find("name")
    for trkseg in trk:nodes("trkseg") do
-      t_points, d_points = calculate_points(trkseg, interval)
+      t_points, d_points = calculate_points(trkseg, 60, 200)
    end
 end
 
