62614750d446143e10bef46661fd1a1bc9232a03
Stephen Irons <stephen.irons@clear.net.nz>
2012-05-04 10:12:18 +1200

    Added function to calculate bearing from A to B.

--- gpx.lua
+++ gpx.lua
@@ -2,6 +2,7 @@
 
 require("LuaXml")
 
+
 file=xml.load(arg[1])
 gpx = file:find("gpx")
 
@@ -25,20 +26,33 @@ end
 spheroid = {}
 spheroid.r = 6378100
 
-function haversine(theta)
-   return math.sin(theta / 2) ^ 2
+function haversin(theta)
+   local theta2 = theta / 2
+   local sin_theta2 = math.sin(theta2)
+   return sin_theta2 * sin_theta2
 end
 
-function f(lat1, lon1, lat2, lon2, sph)
-   local d = sph.r * 2 * math.asin(math.sqrt(haversine(lat2 - lat1) + math.cos(lat1) * math.cos(lat2) * haversine(lon2 - lon1)))
+function distance_between(lat1, lon1, lat2, lon2, sph)
+   local d = sph.r * 2 * math.asin(math.sqrt(haversin(lat2 - lat1) + math.cos(lat1) * math.cos(lat2) * haversin(lon2 - lon1)))
    return d
+end
+
+function bearing(lat1, lon1, lat2, lon2, sph)
+   local dlon = lon2 - lon1
+   local y = math.sin(dlon) * math.cos(lat2)
+   local x = math.cos(lat1) * math.sin(lat2) - math.sin(lat1) * math.cos(lat2) * math.cos(dlon)
+   local bearing = math.atan2(y, x)
+   return math.fmod(bearing + 2*math.pi, 2*math.pi)
 end
 
 
 function rad_from_deg(deg)
-   return deg * 2 * math.pi / 360
+   return deg * math.pi / 180
 end
 
+function deg_from_rad(rad)
+   return rad * 180 / math.pi
+end
 
 function timetable(year, month, day, hour, min, sec)
    local t = {}
@@ -80,8 +94,8 @@ function calculate_points(trkseg, t_interval, d_interval)
       
          if tp2.time ~= tp1.time then
             local delta_t = tp2.time - tp1.time
--- TODO: Calculate bearing            local bearing = 
-            local delta_d = f(tp2.lat, tp2.lon, tp1.lat, tp1.lon, spheroid)
+            local delta_d = distance_between(tp1.lat, tp1.lon, tp2.lat, tp2.lon, spheroid)
+            local bearing = bearing(tp1.lat, tp1.lon, tp2.lat, tp2.lon, spheroid)
             
             if rounded_d >= total_d and rounded_d < total_d + delta_d then
                -- Rounded point is this far along this segment
@@ -89,7 +103,7 @@ function calculate_points(trkseg, t_interval, d_interval)
                print(tp2.time, "*", "*", partial_d, rounded_d)
                rounded_d = rounded_d + d_interval
             end
-            print(tp2.time, total_t, delta_t, total_t + delta_t, total_d, delta_d, total_d+delta_d)
+            print(tp2.time, total_t, delta_t, total_t + delta_t, total_d, delta_d, deg_from_rad(bearing))
 
             total_d = total_d + delta_d
             total_t = total_t + delta_t
