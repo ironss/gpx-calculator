71bb0360071f012291aa248a3a67b4db5a56cbcc
Stephen Irons <stephen.irons@clear.net.nz>
2012-05-03 17:11:09 +1200

    Made trackpoint extraction a function.

--- gpx.lua
+++ gpx.lua
@@ -5,15 +5,15 @@ require("LuaXml")
 file=xml.load(arg[1])
 gpx = file:find("gpx")
 
-function nodes(t, name)
+-- Iterator for searching for nodes with a specific tag
+function xml.nodes(t, tag)
    local i = 0
    local f = function()
       local n
       repeat
          i = i + 1
          n = t[i]
---         print("                                                   ", name, i, n)
-      until (n == nil or n:find(name) ~= nil)
+      until (n == nil or n:find(tag) ~= nil)
       
       return n
    end
@@ -22,15 +22,29 @@ function nodes(t, name)
 end
 
 
-for trk in nodes(gpx, "trk") do
-   trk.name = trk:find("name")
-   print(trk.name)
-   
-   for trkseg in nodes(trk, "trkseg") do
-      for trkpt in nodes(trkseg, "trkpt") do
-         trkpt.time = trkpt:find("time")[1]
-         print(trkpt.time, trkpt.lat, trkpt.lon)
+function gpx_trackpoints(gpx)
+   local t = {}
+   for trk in gpx:nodes("trk") do
+      trk.name = trk:find("name")
+      
+      for trkseg in trk:nodes("trkseg") do
+         for trkpt in trkseg:nodes("trkpt") do
+            tp = {}
+            tp.lat = trkpt.lat
+            tp.lon = trkpt.lon
+            tp.time = trkpt:find("time")[1]
+            t[#t+1] = tp
+         end
       end
    end
+   return t
+end
+
+
+
+t = gpx_trackpoints(gpx)
+for i = 2, #t do 
+   tp = t[i]
+   print(i, tp.time, tp.lat, tp.lon)
 end
 
