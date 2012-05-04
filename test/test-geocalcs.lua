da843c07098649010227e51c4acf65940e47892b
Stephen Irons <stephen.irons@clear.net.nz>
2012-05-04 17:30:53 +1200

    Added another test value.

--- test/test-geocalcs.lua
+++ test/test-geocalcs.lua
@@ -24,6 +24,10 @@ function Test_geocalcs:test_bearing_to()
    assert_close(geo.bearing(0.0, 0.0,  0.0,  0.1, geo.spheroid), math.rad(90), eps)
    assert_close(geo.bearing(0.0, 0.0, -0.1,  0.0, geo.spheroid), math.rad(180), eps)
    assert_close(geo.bearing(0.0, 0.0,  0.0, -0.1, geo.spheroid), math.rad(270), eps)
+
+   assert_close(geo.bearing(math.rad(-43.490587), math.rad(172.564695), 
+                            math.rad(-43.490644), math.rad(172.564605), geo.spheroid),
+                            math.rad(228.87976757496), eps)
 end
 
 
