f0c7a902d366f904c4081bee2e6c17b33f7bdc52
Stephen Irons <stephen.irons@clear.net.nz>
2012-05-07 20:36:28 +1200

    First set of tests for xml.lua.

--- test/test-xml.lua
+++ test/test-xml.lua
@@ -1,14 +1,64 @@
 #! /usr/bin/lua
 
+require('luaunit')
+
 xml = require("xml")
 
-f = io.open('test/2012-04-30-0830-mxmariner.gpx')
-x = f:read('*all')
---print(x)
-xdata = xml.collect(x)
+Test_xml_tostring  = {}
+
+
+function Test_xml_tostring:test_empty()
+   local t1 = { [0]='test' }
+   local actual = xml.tostring(t1)
+   assertEquals(actual, '<test />\n')
+end
+
+function Test_xml_tostring:test_empty_with_attributes()
+   local t1 = { [0]='test', name='Name', desc='Description' }
+   local actual = xml.tostring(t1)
+   assertEquals(actual, '<test name="Name" desc="Description" />\n')
+end
+
+function Test_xml_tostring:test_with_string_data()
+   local t1 = { [0]='test', 'Test data 1' }
+   local actual = xml.tostring(t1)
+   assertEquals(actual, '<test>Test data 1</test>\n')
+end
+
+function Test_xml_tostring:test_with_nested_data()
+   local t1 = { [0]='test' }
+   local t2 = { [0]='nest' }
+   t1[1] = t2
+   local actual = xml.tostring(t1)
+   assertEquals(actual, [[
+<test>
+  <nest />
+</test>
+]])
+end
+
+function Test_xml_tostring:test_with_all()
+   local t1 = { [0]='test' }
+   local t2 = { [0]='nest' }
+   local t3 = { [0]='more', name='Name' }
+   t1[1] = t2
+   t1[2] = "Test data"
+   t1[3] = "More data"
+   t1[4] = t3
+   t1[5] = "Still more"
+   local actual = xml.tostring(t1)
+   assertEquals(actual, [[
+<test>
+  <nest />
+  Test data 
+  More data 
+  <more name="Name" />
+  Still more 
+</test>
+]])
+end
 
 
 
-t = require('table-tostring')
-print(t.to_string(xdata))
+LuaUnit:run()
 
