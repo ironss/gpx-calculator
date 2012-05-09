904097c476244d2fb547327bdbbb1cc2bdc4bac1
Stephen Irons <stephen.irons@clear.net.nz>
2012-05-09 18:28:10 +1200

    Added new test.

--- test/dateparse-tests.lua
+++ test/dateparse-tests.lua
@@ -6,10 +6,12 @@ require("luaunit")
 Test_dateparse = {}
 
 function Test_dateparse:test_date_only()
-   assertEquals(parsedate('2012')      , 1325329200)
-   assertEquals(parsedate('201204')    , 1333191600)
-   assertEquals(parsedate('20120421')  , 1334923200)
-   assertEquals(parsedate('2012-04-21'), 1334923200)
+   assertEquals(parsedate('2012')         , 1325329200)
+   assertEquals(parsedate('201204')       , 1333191600)
+   assertEquals(parsedate('20120421')     , 1334923200)
+   assertEquals(parsedate('2012-04-21')   , 1334923200)
+   assertEquals(parsedate('21 April 2012'), 1334923200)
+--   assertEquals(parsedate('April 2012')   , 1333191600)
 end
 
 function Test_dateparse:test_combined()
@@ -30,6 +32,7 @@ end
 
 function Test_dateparse:disabled_test_timezone_offset()
    assertEquals(parsedate('2012-04-21 01:02:03+12'), 1334926923)
+   assertEquals(parsedate('2012-04-21 01:02:03+1200'), 1334926923)
 end
 
 LuaUnit:run()
