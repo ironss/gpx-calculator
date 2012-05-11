eeb9551266bc1c11e175b95d02621761e275fa4c
Stephen Irons <stephen.irons@clear.net.nz>
2012-05-11 23:34:10 +1200

    Added real performance tests.

--- performance.lua
+++ performance.lua
@@ -11,14 +11,8 @@ local clock_gettime
 local clock_resolution
 
 local posix = require 'posix'
-if posix and posix.clock_gettime_ then
-   clock_resolution = 0.01
-
-   -- gettime
-   -- Returns monotonic increasing time as decimal seconds.
-   -- Resolution depends on underlying POSIX library, 
-   --   could be 10ms, or 1us or even 1ns
-
+if posix and posix.clock_gettime then
+   clock_resolution = 0.001  -- How can we read the resolution of the POSIX clocks?
    function clock_gettime()
       local s, f = posix.clock_gettime("")
       local t = s + f / 1000000000
@@ -47,6 +41,7 @@ end
 
 M.null_calibration = { }
 M.gettime_calibration = { }
+M.calibration_accuracy = 1
 
 local function measure(f, accuracy)
    if accuracy == 0 or accuracy == nil then
@@ -54,15 +49,13 @@ local function measure(f, accuracy)
    end
    local max_t = 1/accuracy * clock_resolution
    local max_n = 1/accuracy
-   print(accuracy, max_n, max_t)
    
-   local t_total
-   local n
-   
-   if M.null_calibration.ave == nil and M.gettime_calibration.ave == nil then
-      M.calibrate()
+   if accuracy < M.calibration_accuracy then
+      M.calibrate(accuracy)
    end
 
+   local t_total
+   local n
 
    local t_1
    local t_2
@@ -76,17 +69,21 @@ local function measure(f, accuracy)
    until (t_2 >= t_1 + max_t) or (n >= max_n and t_2 >= t_1 + clock_resolution)
    t_total = t_2 - t_1
  
-   return { t_total=t_total, n=n, ave=(t_total / n) }
+   return { t_total=t_total, n=n, t_ave=(t_total / n) }
 end
 
 
-local function calibrate(n)
+local function calibrate(n, accuracy)
+   if accuracy == 0 or accuracy == nil then
+      accuracy = 0.00001
+   end
+
    M.null_calibration.ave = 0
    M.gettime_calibration.ave = 0
-   M.null_calibration = measure(function() end)
-   M.gettime_calibration = measure(clock_gettime)
+   M.calibration_accuracy = accuracy
+   M.null_calibration = measure(function() end, accuracy)
+   M.gettime_calibration = measure(clock_gettime, accuracy)
 end
-
 
 M.clock_resolution = clock_resolution
 M.gettime = gettime
--- test/test-performance.lua
+++ test/test-performance.lua
@@ -6,6 +6,48 @@ print('Calibrating...')
 
 p.calibrate()
 print('Resolution: ', p.clock_resolution)
-print('null() overhead: ', p.null_calibration.ave, p.null_calibration.t_total, p.null_calibration.n)
-print('gettime() overhead: ', p.gettime_calibration.ave, p.gettime_calibration.t_total, p.gettime_calibration.n)
+print('null() overhead: ', p.null_calibration.t_ave, p.null_calibration.t_total, p.null_calibration.n)
+print('gettime() overhead: ', p.gettime_calibration.t_ave, p.gettime_calibration.t_total, p.gettime_calibration.n)
+
+
+tests = 
+{
+   { '1ms'  , 0.001 },
+   { '2ms'  , 0.002 },
+   { '5ms'  , 0.005 },
+   { '10ms' , 0.01  },
+   { '20ms' , 0.02  },
+   { '50ms' , 0.05  },
+   { '100ms', 0.1   },
+   { '200ms', 0.2   },
+   { '500ms', 0.5   },
+   { '1s'   , 1     },
+   { '2s'   , 2     },
+   { '5s'   , 5     },
+}
+
+
+require 'posix'
+
+function clock_gettime()
+   local s, f = posix.clock_gettime("")
+   local t = s + f / 1000000000
+   return t
+end
+
+function busy_loop(t)
+   local t_1
+   local t_2
+   t_1 = clock_gettime()
+   repeat
+      t_2 = clock_gettime()
+   until t_2 > t_1 + t
+end
+
+
+for _, t in ipairs(tests) do
+   print(t[1])
+   t.m = p.measure(function() busy_loop(t[2]) end, 0.01)
+   print(t[1] .. ': ', t.m.t_total, t.m.n, t.m.t_ave)
+end
 
