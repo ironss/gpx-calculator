e7d99cf246b70202ffd4a6fa22e9aa00962e2544
Stephen Irons <stephen.irons@clear.net.nz>
2012-06-21 07:19:24 +1200

    Added real support for ISO8601 time format.

--- test/dateparse-tests.lua
+++ test/dateparse-tests.lua
@@ -19,6 +19,15 @@ function Test_dateparse:test_combined()
    assertEquals(parsedate('2012-04-21 01:02:03'), 1334926923)
 end
 
+function Test_dateparse:test_fractional_second()
+   assertEquals(parsedate('20120421010203.0'), 1334926923)
+--   assertEquals(parsedate('20120421010203.5'), 1334926923.5)
+   assertEquals(parsedate('2012-04-21T01:02:03.0'), 1334926923)
+   assertEquals(parsedate('2012-04-21T01:02:03.00'), 1334926923)
+   assertEquals(parsedate('2012-04-21T01:02:03.000'), 1334926923)
+   assertEquals(parsedate('2012-04-21T01:02:03.0000'), 1334926923)
+end
+
 function Test_dateparse:test_timezone_military()
    assertEquals(parsedate('2012-04-21T01:02:03Z'), 1334926923)
    assertEquals(parsedate('2012-04-21 01:02:03Z'), 1334926923)
@@ -26,13 +35,14 @@ function Test_dateparse:test_timezone_military()
 end
 
 function Test_dateparse:test_timezone_nz()
-   assertEquals(parsedate('2012-04-21 01:02:03NZST'), 1334926923)
+   assertEquals(parsedate('2012-04-21T01:02:03NZST'), 1334926923)
    assertEquals(parsedate('2012-04-21 01:02:03NZDT'), 1334926923)
 end
 
-function Test_dateparse:disabled_test_timezone_offset()
-   assertEquals(parsedate('2012-04-21 01:02:03+12'), 1334926923)
-   assertEquals(parsedate('2012-04-21 01:02:03+1200'), 1334926923)
+function Test_dateparse:test_timezone_offset()
+   --assertEquals(parsedate('2012-04-21T01:02:03+12'), 1334926923)
+   assertEquals(parsedate('2012-04-21T01:02:03+1200'), 1334926923)
+   assertEquals(parsedate('2012-04-21T01:02:03+12:00'), 1334926923)
 end
 
 LuaUnit:run()
